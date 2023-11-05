0032de3648e50d16895ff8908fc76d45aad1deda
Husain Fadhilah <husainazkas@gmail.com>
2023-11-06 04:34:42 +0800

    chore: remove # on url suffix

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 import 'firebase_options.dart';
 import 'injector.dart';
@@ -14,6 +15,8 @@ Future<void> main() async {
   if (kDebugMode) {
     Bloc.observer = SimpleBlocObserver();
   }
+
+  usePathUrlStrategy();
 
   await initLocator();
 
