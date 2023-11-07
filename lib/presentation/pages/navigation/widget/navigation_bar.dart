a1730cda6766dde458dddb90d07f7fa4077dfd18
Husain Fadhilah <husainazkas@gmail.com>
2023-11-07 14:14:54 +0800

    feat: update account button

--- lib/presentation/pages/navigation/widget/navigation_bar.dart
+++ lib/presentation/pages/navigation/widget/navigation_bar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
-import '../../../../domain/core/utils.dart';
 import '../../../blocs/auth/auth_bloc.dart';
 import '../../../blocs/common/loading/loading_bloc.dart';
 import '../../login/login_dialog.dart';
@@ -63,32 +62,71 @@ class NavigationBar extends StatelessWidget implements PreferredSizeWidget {
           Align(
             alignment: Alignment.centerRight,
             child: BlocBuilder<AuthBloc, AuthState>(
-              builder: (context, state) => _buildButton(
-                context,
-                label: state.maybeWhen(
-                  authenticated: (user) =>
-                      '${user.displayName?.truncate(10) ?? ''}\nKeluar'.trim(),
-                  orElse: () => 'Masuk',
-                ),
-                onPressed: () {
-                  context.read<AuthBloc>().state.maybeMap(
-                        authenticated: (_) {
-                          context
-                              .read<AuthBloc>()
-                              .add(const AuthEvent.signedOut());
+              builder: (context, state) => state.maybeWhen(
+                authenticated: (user) => Material(
+                  shape: const CircleBorder(),
+                  clipBehavior: Clip.hardEdge,
+                  child: PopupMenuButton<int>(
+                    tooltip: 'Akun Saya',
+                    position: PopupMenuPosition.under,
+                    itemBuilder: (_) => [
+                      PopupMenuItem(
+                        value: 0,
+                        child: DefaultTextStyle.merge(
+                          style: const TextStyle(fontSize: 14.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                user.displayName ?? 'Anonymous',
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              if (user.email != null) Text(user.email!)
+                            ],
+                          ),
+                        ),
+                      ),
+                      const PopupMenuItem(
+                        value: 1,
+                        child: Text(
+                          'Keluar',
+                          style: TextStyle(fontSize: 14.0),
+                        ),
+                      ),
+                    ],
+                    onSelected: (value) {
+                      if (value == 1) {
+                        context
+                            .read<AuthBloc>()
+                            .add(const AuthEvent.signedOut());
+                        context
+                            .read<LoadingBloc>()
+                            .add(const LoadingEvent.started());
+                        Future.delayed(const Duration(milliseconds: 1000), () {
                           context
                               .read<LoadingBloc>()
-                              .add(const LoadingEvent.started());
-                          Future.delayed(const Duration(milliseconds: 1000),
-                              () {
-                            context
-                                .read<LoadingBloc>()
-                                .add(const LoadingEvent.stopped());
-                          });
-                        },
-                        orElse: () => showLoginDialog(context),
-                      );
-                },
+                              .add(const LoadingEvent.stopped());
+                        });
+                      }
+                    },
+                    child: CircleAvatar(
+                      backgroundImage: user.photoURL == null
+                          ? null
+                          : NetworkImage(user.photoURL!),
+                      backgroundColor: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
+                ),
+                unauthenticated: () => _buildButton(
+                  context,
+                  label: 'Masuk',
+                  onPressed: () {
+                    showLoginDialog(context);
+                  },
+                ),
+                orElse: () => const SizedBox(),
               ),
             ),
           ),
