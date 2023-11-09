55f42e623b416a845053fdfc70cb18131c4c1663
Husain Fadhilah <husainazkas@gmail.com>
2023-11-09 14:56:53 +0800

    perf: improve login action

--- lib/presentation/blocs/auth/auth_state.dart
+++ lib/presentation/blocs/auth/auth_state.dart
@@ -2,7 +2,14 @@ part of 'auth_bloc.dart';
 
 @freezed
 class AuthState with _$AuthState {
+  const AuthState._();
+
   const factory AuthState.initial() = _Initial;
   const factory AuthState.unauthenticated() = _Unauthenticated;
   const factory AuthState.authenticated(User user) = _Authenticated;
+
+  bool get isAuthenticated => maybeWhen(
+        authenticated: (_) => true,
+        orElse: () => false,
+      );
 }
--- lib/presentation/blocs/common/loading/loading_state.dart
+++ lib/presentation/blocs/common/loading/loading_state.dart
@@ -2,6 +2,10 @@ part of 'loading_bloc.dart';
 
 @freezed
 class LoadingState with _$LoadingState {
+  const LoadingState._();
+
   const factory LoadingState.running([String? message]) = _LoadingRunning;
   const factory LoadingState.stopped() = _LoadingStopped;
+
+  bool get isLoading => when(running: (_) => true, stopped: () => false);
 }
